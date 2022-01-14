25347087b5daf3aa75951fe102a16d05d83ef6ed
earl-stephens <stephensearl@hotmail.com>
2022-01-13 21:55:27 -0700

    Add acceptance criteria

---
+++ src/test/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+package test;
+
+/* Write a program that has one method test(int value)
+ * For multiples of three print "Fizz" instead of the
+ * number.  For multiples of five print "Buzz".  
+ * Anything else simply returns the number.
+ */
+
+public class FizzBuzzTest {
+
+}
